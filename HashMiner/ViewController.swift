bb10057967e0596ce3b7a2907f841e2f940c7d8e
Hangyu Ye <jerry.hangyu@gmail.com>
2019-09-11 23:08:40 +0800

    Initial Commit

---
+++ HashMiner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HashMiner
+//
+//  Created by Jerry on 9/11/19.
+//  Copyright © 2019 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
