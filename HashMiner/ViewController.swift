e3c78bafe6fc14e3c656dfcf2378181a2834fbf4
Hangyu Ye <jerry.hangyu@gmail.com>
2019-09-12 21:28:22 +0800

    Convert to DoubleSHA

--- HashMiner/ViewController.swift
+++ HashMiner/ViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hashPowerLabel: UILabel!
+    
+    
     var targetStr = "Hello, Swift"
-    var hashResult: String!
+    var array: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     func prograssHash() {
         let start_time = Date().timeIntervalSince1970
         for _ in 1...50000 {
-            hashResult = targetStr.sha256()
+            array.append((targetStr.sha256().sha256()))
         }
         let used_time = Date().timeIntervalSince1970 - start_time
-        print(50000/used_time)
+        hashPowerLabel.text = "Hash Power: \(Double(array.count)/used_time) h/s"
     }
 
     @IBAction func startBtnPressed(_ sender: Any) {
+        array = []
         prograssHash()
     }
 }
